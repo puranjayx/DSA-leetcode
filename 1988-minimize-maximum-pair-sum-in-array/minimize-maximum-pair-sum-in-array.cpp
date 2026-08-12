c1a0b7c1b5e565c6b45442491ebb10267584dfcb
codedbyPuranjay <puranjaygupta413@gmail.com>
2026-08-13 01:49:28 +0530

    Time: 183 ms (14.51%) | Memory: 105.6 MB (5.01%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -5,13 +5,13 @@ public:
         int low=0;
         int high=nums.size()-1;
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            if(low<high){
+        
+            while(low<high){
             ans.push_back(nums[low]+nums[high]);
             low++;
             high--;
             }
-        }
+        
         int maxele=INT_MIN;
         for(auto i : ans){
             maxele=max(i,maxele);
