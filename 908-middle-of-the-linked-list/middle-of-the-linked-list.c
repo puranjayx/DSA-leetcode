0dacb7ce1126b0f001a1d32cdce9afd5fbbf86d5
codedbyPuranjay <puranjaygupta413@gmail.com>
2026-09-11 20:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (67.69%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.c
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.c
@@ -6,18 +6,25 @@
  * };
  */
 struct ListNode* middleNode(struct ListNode* head) {
-    int count;
-      struct ListNode * temp=head;
-     while(temp!=NULL){
-        temp=temp->next;
-        count++;
-     }
-    int middle = count/2;
-       temp=head;
-       int i =0;
-     while(i!=middle && temp!=NULL){
-        temp=temp->next;
-        i++;
-     }
-     return temp;
+    // int count;
+    //   struct ListNode * temp=head;
+    //  while(temp!=NULL){
+    //     temp=temp->next;
+    //     count++;
+    //  }
+    // int middle = count/2;
+    //    temp=head;
+    //    int i =0;
+    //  while(i!=middle && temp!=NULL){
+    //     temp=temp->next;
+    //     i++;
+    //  }
+    //  return temp;
+    struct ListNode * fast=head;
+    struct ListNode * slow=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
 }
